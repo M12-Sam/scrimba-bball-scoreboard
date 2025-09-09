ccf77f4bcae75769c56ba1623fe56c62baa1b9b7
M12-Sam <sam@mach12-consulting.com>
2025-09-09 18:37:16 +0800

    commit 3 (npm install done)

--- index.js
+++ index.js
@@ -1,37 +1,37 @@
-let homescoreEl = document.getElementById("homescore-el")
-let guestscoreEl = document.getElementById("guestscore-el")
-let inithome = 0
-let initguest = 0
+let homescoreEl = document.getElementById("homescore-el");
+let guestscoreEl = document.getElementById("guestscore-el");
+let inithome = 0;
+let initguest = 0;
 
-console.log(homescoreEl.textContent)
-console.log(guestscoreEl.textContent)
+console.log(homescoreEl.textContent);
+console.log(guestscoreEl.textContent);
 
 function homep1() {
-    inithome +=1
-    homescoreEl.textContent = inithome    
+  inithome += 1;
+  homescoreEl.textContent = inithome;
 }
 
 function homep2() {
-    inithome +=2
-    homescoreEl.textContent = inithome    
+  inithome += 2;
+  homescoreEl.textContent = inithome;
 }
 
 function homep3() {
-    inithome +=3
-    homescoreEl.textContent = inithome    
+  inithome += 3;
+  homescoreEl.textContent = inithome;
 }
 
 function guestp1() {
-    initguest +=1
-    guestscoreEl.textContent = initguest   
+  initguest += 1;
+  guestscoreEl.textContent = initguest;
 }
 
 function guestp2() {
-    initguest +=2
-    guestscoreEl.textContent = initguest  
+  initguest += 2;
+  guestscoreEl.textContent = initguest;
 }
 
 function guestp3() {
-    initguest +=3
-    guestscoreEl.textContent = initguest 
+  initguest += 3;
+  guestscoreEl.textContent = initguest;
 }
